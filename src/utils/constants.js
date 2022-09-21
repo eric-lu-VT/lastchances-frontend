2b20f5063311501899f863bf0b17ab95e00a2243
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-09-20 20:10:57 -0400

    change back to prod url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = 'http://localhost:4000/'
-// export const SERVER_URL = 'https://lastchances-backend.onrender.com/'
+// export const SERVER_URL = 'http://localhost:4000/'
+export const SERVER_URL = 'https://lastchances-backend.onrender.com/'
 
 export const ROUTES = {
   HOME: '/',
